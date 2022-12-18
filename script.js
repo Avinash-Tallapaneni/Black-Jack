57b11826bb1c2ba06c172dac97c0741e7027d1b2
Avinash Tallapaneni <avinashtallapaneni@gmail.com>
2022-12-19 00:25:49 +0530

    Changes made to Javascript and css

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-try {
 let dealerSum = 0;
 let playerSum = 0;
 let playerAce = 0;
 let dealerAce = 0;
-var canHit = true;
+let canHit = true;
 var deck;
-
 
 window.onload = function() {
     createDeck();
@@ -13,7 +11,15 @@ window.onload = function() {
     startGame();
 }
 
+const displayMessage = {
+    0 : "You lose!, Try again", //dealer sum is greater than player
+    1 : "congradualation, You Win!", // player sum is greater than dealer
+    2 : "ahhh, Its a Tie", //if both have same value
+}
 
+
+const stayEl = document.getElementById("stay");
+const hitEl = document.getElementById("hit");
 
 //Card values and types
 let card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -54,11 +60,14 @@ const startGame = () => {
 
         let dealercardImg = document.createElement("img");
         let dealerOpeningCards = deck.pop(); // return string
+        console.log(dealerOpeningCards, 'dealer opening card')
         dealercardImg.src = "./Black_jack_cards/" + dealerOpeningCards+".png";
         document.getElementById("dealerOpeningCardsImg").append(dealercardImg);
+        document.getElementById("dealerOpeningCardsImg").style.display = 'none';
 
-        dealerSum += openingCards(dealerOpeningCards);
-        console.log("dealerSum :" + dealerSum);
+
+        dealerSum += openingCards(dealerOpeningCards,"dealer");
+
 
 
         let playerCardImg = document.createElement("img");
@@ -66,37 +75,42 @@ const startGame = () => {
         playerCardImg.src = "./Black_jack_cards/" + playerOpeningCards + ".png";
         document.getElementById("playerOpeningCardsImg").append(playerCardImg);
 
+
         playerSum += openingCards(playerOpeningCards);
-        console.log("playerSum :" + playerSum);
+
 
         // console.log(playerCardImg.src);
 
     }
+        console.log("dealerSum :" + dealerSum);
+        console.log("playerSum :" + playerSum);
         while(dealerSum < 17) {
 
             let dealercardImg = document.createElement("img");
-            let dealerOpeningCards = deck.shift(); // return string
+            let dealerOpeningCards = deck.pop(); // return string
             dealercardImg.src = "./Black_jack_cards/" + dealerOpeningCards+".png";
             document.getElementById("dealerOpeningCardsImg").append(dealercardImg);
-            dealerSum += openingCards(dealerOpeningCards);
+            dealerSum += openingCards(dealerOpeningCards,"dealer");
             console.log("dealerSum :" + dealerSum);
     }
 
     document.getElementById("hit").addEventListener("click", hit);
     document.getElementById("stay").addEventListener("click", stay);
-
 }
 
+
+
+
 //returns cardsvalues for playersum
-const openingCards = (el) => {
-    let cardSplit = el.split(" - "); // returns string in an array. ["A", "7"]
+const openingCards = (el,person = "player") => {
+    let cardSplit = el.split("-"); // returns string in an array. ["A", "7"]
     let cardNumberDigit = (cardSplit[0]); // return only first value of an array
     // console.log(cardNumberDigit);
 
     if(isNaN(cardNumberDigit)) { //assign value of 10 to face cards
         if(cardNumberDigit === "A") {
             cardNumberDigit = 11; // Ace value is assigned 11
-            playerAce += 1;
+            person === 'dealer' ? ++dealerAce : ++playerAce;
         } else cardNumberDigit = 10;
     }
     // console.log(parseInt(cardNumberDigit));
@@ -109,6 +123,16 @@ const openingCards = (el) => {
 // player Hit or stay
 
 const hit = () => {
+    console.log(playerSum, 'in hit playersum')
+    if(dealerSum > 22) {
+        aceChange();
+        // if(dealerSum > 22) {
+        // message = displayMessage[1];
+        // document.querySelectorAll(".result")[0].textContent= message;
+
+
+        // }
+    }
 
     if(!canHit) {
         return
@@ -120,66 +144,92 @@ const hit = () => {
     document.getElementById("playerOpeningCardsImg").append(playerCardImg);
     playerSum += openingCards(value);
     console.log("playerSum :" + playerSum);
+    // console.log("playerAce :" + playerAce);
 
-    // console.log(playerAce);
-    if(playerSum > 22 || dealerSum >22) {
+
+  if(playerSum >= 22) {
         aceChange();
+  }
+
+  if (playerSum >= 22){
+    if(playerSum == 21){
+
+        message = displayMessage[1];
         canHit = false;
+        document.querySelectorAll(".dealer-header")[0].textContent = dealerSum;
+        document.querySelectorAll(".player-header")[0].textContent= playerSum;
+        document.querySelectorAll(".result")[0].textContent= message;
+
+        document.getElementById("dealerOpeningCardsImg").style.display = "";
+        document.getElementById("dealerHiddenCard").style.display = "none";
+
+        stayEl.textContent = "New Game";
+        hitEl.style.display = "none";
+        stayEl.addEventListener("click", restart);
     }
-    console.log("playerSum ace:" + playerSum);
-    console.log("playerAce :" + playerAce);
+    message = displayMessage[0];
+    canHit = false;
+        document.querySelectorAll(".dealer-header")[0].textContent = dealerSum;
+        document.querySelectorAll(".player-header")[0].textContent= playerSum;
+        document.querySelectorAll(".result")[0].textContent= message;
+
+        document.getElementById("dealerOpeningCardsImg").style.display = "";
+        document.getElementById("dealerHiddenCard").style.display = "none";
+
+        stayEl.textContent = "New Game";
+        hitEl.style.display = "none";
+        stayEl.addEventListener("click", restart);
+
+    }
 
 }
+
+
+
 
 
 const stay = () => {
-    let card = deck.shift()
-    dealerSum += openingCards(card);
     canHit = false;
-    console.log("dealerSum :" + dealerSum);
-    // console.log(playerAce);
+
     if(dealerSum > 22) {
         aceChange();
     }
-    console.log("playerSum ace:" + playerSum);
-    console.log("playerAce :" + playerAce);
 
-
-
-    let message = "";
-    const displayMessage = {
-        0 : "You lose!, Try again", //dealer sum is greater than player
-        1 : "congradualation, You Win!", // player sum is greater than dealer
-        2 : "ahhh, Its a Tie", //if both have same value
-    }
-    if(playerSum > 21 || playerSum < dealerSum) {
-        message = displayMessage[0];
-    } else if (playerSum == dealerSum) {
+    if(playerSum == dealerSum || (playerSum > 21 && dealerSum > 21)) {
         message = displayMessage[2];
-
-    } else if (dealerSum > 21 || playerSum > dealerSum) {
+    } else if (playerSum <= 21 && (playerSum > dealerSum || dealerSum >21)) {
         message = displayMessage[1];
 
+    } else  {
+        message = displayMessage[0];
+
     }
+    document.querySelectorAll(".dealer-header")[0].textContent = dealerSum;
+    document.querySelectorAll(".player-header")[0].textContent= playerSum;
+    document.querySelectorAll(".result")[0].textContent= message;
 
-    // document.querySelectorAll(".dealer-header").innerText = dealerSum;
-    // document.querySelectorAll(".player-header").innerText = playerSum;
-    // document.querySelectorAll(".result").innerText = message;
-    document.querySelectorAll(".dealer-header")[0].innerText = dealerSum;
-    document.querySelectorAll(".player-header")[0].innerText = playerSum;
-    document.querySelectorAll(".result")[0].innerText = message;
 
+    document.getElementById("dealerOpeningCardsImg").style.display = "";
+    document.getElementById("dealerHiddenCard").style.display = "none";
+
+    stayEl.textContent = "New Game";
+    hitEl.style.display = "none";
+    stayEl.addEventListener("click", restart);
+    // document.getElementById("dealerOpeningCardsImg").style.display = "";
+    // document.getElementById("dealerHiddenCard").style.display = "none ";
 
 }
 
+
 const aceChange = () => {
     if(playerSum > 22) {
-     while(playerAce >= 1 && playerSum > 21 ) {
+     while(playerAce >= 1 && playerSum > 22 ) {
      playerSum -= 10;
      playerAce -= 1;
     }
-    } else {
-        while(dealerAce >= 1 && dealerSum > 21 ) {
+
+    if (dealerSum > 22)
+        while(dealerAce >= 1 && dealerSum > 22 ) {
         dealerSum -= 10;
         dealerAce -= 1;
     }
@@ -188,15 +238,33 @@ const aceChange = () => {
 
 }
 
-}catch (error) {
-    // if error true => console.log(err)
-    console.error(error);
-  }
+
+const restart = () => {
+
+
+    document.getElementById("dealerOpeningCardsImg").innerHTML ="";
+    document.getElementById("playerOpeningCardsImg").innerHTML ="";
+    document.getElementById("dealerHiddenCard").style.display = "";
+    document.querySelectorAll(".dealer-header")[0].innerText = "Dealer"
+    document.querySelectorAll(".player-header")[0].innerText = "player"
+    document.querySelectorAll(".result")[0].innerText = "";
+    hitEl.style.display = "";
 
 
 
+    dealerSum = 0;
+    playerSum = 0;
+    playerAce = 0;
+    dealerAce = 0;
+    canHit = true;
 
+    stayEl.textContent = "Stay"
+    //  hitEl.textContent = "hit"
 
+    stayEl.removeEventListener ("click", restart);
+    hitEl.removeEventListener ("click", restart);
+    startGame();
+    createDeck();
+    shuffleDeck();
 
-
-
+}
